ace01e964a00a9be5a139448628a1fdff5dd0f66
acb16ua <leouddhav@gmail.com>
2019-03-25 16:51:25 +0000

    made a login page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sniped!' });
+});
+
+router.post('/profile', function (req, res, next) {
+  var login = req.body.login;
+  var password = req.body.password;
+
+
 });
 
 module.exports = router;
