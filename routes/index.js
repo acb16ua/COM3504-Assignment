1bbdaea602f5576063c512b80272080ca4786e35
Philip D'Souza <philip.a.dsouza@gmail.com>
2019-03-29 18:36:19 +0000

    Multer- image upload and add event button

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
+const multer = require('multer');
+const upload = multer({dest: 'private/uploads'});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sniped!', login_is_correct: true });
 });
 
+/* POST login info. */
 router.post('/', function (req, res, next) {
   var login = req.body.login;
   var password = req.body.password;
@@ -15,9 +19,18 @@ router.post('/', function (req, res, next) {
 });
 
 
-/* GET home page. */
+/* GET profile page. */
 router.get('/profile', function(req, res, next) {
     res.render('profile', { title: 'Welcome back!', login_is_correct: true });
 });
+
+/* POST event image. */
+router.post('/upload', upload.single('photo'), (req, res) => {
+    if(req.file) {
+        res.json(req.file);
+    }
+    else throw 'error';
+});
+
 
 module.exports = router;
