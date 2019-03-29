2d03a7f939a9b578c9738a024c5a1d9a42771706
Philip D'Souza <philip.a.dsouza@gmail.com>
2019-03-29 16:36:53 +0000

    Profile page

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sniped!', login_is_correct: true });
 });
 
-router.post('/profile', function (req, res, next) {
+router.post('/', function (req, res, next) {
   var login = req.body.login;
   var password = req.body.password;
 
+});
+
+
+/* GET home page. */
+router.get('/profile', function(req, res, next) {
+    res.render('profile', { title: 'Welcome back!', login_is_correct: true });
 });
 
 module.exports = router;
