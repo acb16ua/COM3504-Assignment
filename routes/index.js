3f7d2f3fac174f831b6a5556833d1292f93cbb1b
Philip D'Souza <philip.a.dsouza@gmail.com>
2019-05-15 15:55:05 +0100

    Added Multer using MongoDB

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 const multer = require('multer');
-const upload = multer({dest: 'private/uploads'});
+var assert = require('assert')
+
+/* Adds mongodb package */
+const MongoClient = require('mongodb').MongoClient;
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'com3504';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+
+/* Renames Multer file upload with dile name and data */
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+
+var upload = multer({storage: storage});
+
 
 
 /* GET home page. */
@@ -25,12 +51,29 @@ router.get('/profile', function(req, res, next) {
 });
 
 /* POST event image. */
-router.post('/upload', upload.single('photo'), (req, res) => {
-    if(req.file) {
-        res.json(req.file);
-    }
-    else throw 'error';
+router.post('/upload', upload.single('photo'), (req, res, next) => {
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+         const db = client.db(dbName);
+        insertDocuments(db, 'public/uploads/' + req.file.filename, () => {
+            client.close();
+            res.json({'message': 'File uploaded successfully'});
+        });
+    });
 });
-
-
 module.exports = router;
+
+/* Function to inset one file to upload collection */
+const insertDocuments = function(db,filePath, callback) {
+    // Get the documents collection
+    const collection = db.collection('upload');
+    // Insert some documents
+    collection.insertOne({'imagePath':filePath}, function(err, result) {
+        assert.equal(err, null);
+        callback(result);
+    });
+}
+
+
+
