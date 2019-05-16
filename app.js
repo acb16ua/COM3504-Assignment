478761816614f6b751e6ddbda623c9bcbf4a3f1a
Philip D'Souza <philip.a.dsouza@gmail.com>
2019-05-16 23:08:06 +0100

    More data

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ function Setup(){
 
 
     var json1 = [
-        {"imagePath":"public/uploads/photo-1558014347932.jpg","profileID":"109272662587431712473"},
-        {"imagePath":"public/uploads/photo-1558014355718.jpg"},
-        {"imagePath":"public/uploads/photo-1558014361498.jpg"},
-        {"imagePath":"public/uploads/photo-1558014368686.jpg"},
-        {"imagePath":"public/uploads/photo-1558014377366.jpg"},
-        {"imagePath":"public/uploads/photo-1558014383941.jpg"},
-        {"imagePath":"public/uploads/photo-1558014390268.jpg"},
-        {"imagePath":"public/uploads/photo-1558014400975.jpg"},
-        {"imagePath":"public/uploads/photo-1558014407541.jpg"},
-        {"imagePath":"public/uploads/photo-1558014414354.jpg"},
-        {"imagePath":"public/uploads/photo-1558014420321.jpg"},
-        {"imagePath":"public/uploads/photo-1558014427095.jpg"},
-        {"imagePath":"public/uploads/photo-1558014434752.jpg"},
-        {"imagePath":"public/uploads/photo-1558014441701.jpg"},
-        {"imagePath":"public/uploads/photo-1558014449385.jpg"},
-        {"imagePath":"public/uploads/photo-1558014459099.jpg"},
-        {"imagePath":"public/uploads/photo-1558014466729.jpg"},
-        {"imagePath":"public/uploads/photo-1558014480929.jpg"},
-        {"imagePath":"public/uploads/photo-1558014487491.jpg"},
-        {"imagePath":"public/uploads/photo-1558014496801.jpg"}
+        {"imagePath":"public/uploads/photo-1558014347932.jpg","profileID":"109272662587431712473","event":"Glasto","comment":"This is glasto","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014355718.jpg","profileID":"109272662587431712473","event":"Cohella","comment":"This is Cohella","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014361498.jpg","profileID":"109272662587431712473","event":"Leeds fest","comment":"This is Leeds fest","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014368686.jpg","profileID":"109272662587431712473","event":"Wireless","comment":"This is Wireless","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014377366.jpg","profileID":"109272662587431712473","event":"Woo Hah0","comment":"This is Woo Hah0","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014383941.jpg","profileID":"109272662587431712473","event":"Reading","comment":"This is Reading","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014390268.jpg","profileID":"109272662587431712473","event":"Londom fest","comment":"This is Londom","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014400975.jpg","profileID":"109272662587431712473","event":"Tramlines","comment":"This is Tramlines","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014407541.jpg","profileID":"109272662587431712473","event":"Didsbury fest","comment":"This is Didsbury","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014414354.jpg","profileID":"109272662587431712473","event":"Junaids world","comment":"This is Junaids","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014420321.jpg","profileID":"109272662587431712473","event":"Outlook","comment":"This is glasto","location":"Outlook","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014427095.jpg","profileID":"109272662587431712473","event":"Social club","comment":"This is Social","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014434752.jpg","profileID":"109272662587431712473","event":"Manchesr academy","comment":"This is Manchesr","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014441701.jpg","profileID":"109272662587431712473","event":"Birmingham disco","comment":"This is disco","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014449385.jpg","profileID":"109272662587431712473","event":"New york party","comment":"This is york","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014459099.jpg","profileID":"109272662587431712473","event":"Japan party","comment":"This is Japan","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014466729.jpg","profileID":"109272662587431712473","event":"Glasto v2","comment":"This is v2","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014480929.jpg","profileID":"109272662587431712473","event":"Austrila partyier","comment":"This is partyier","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014487491.jpg","profileID":"109272662587431712473","event":"India festival","comment":"This is India","location":"","dateTime":"06/12/9412:15"},
+        {"imagePath":"public/uploads/photo-1558014496801.jpg","profileID":"109272662587431712473","event":"Last festival location","comment":"This is Last","location":"","dateTime":"06/12/9412:15"}
     ]
 
     /* Adds mongodb package */
