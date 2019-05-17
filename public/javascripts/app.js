d9a3381693c9a9729be1ff370faa620742f56131
Philip D'Souza <philip.a.dsouza@gmail.com>
2019-05-17 23:46:57 +0100

    last

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,27 +1,42 @@
+function initIndexSniped() {
+    onSubmit();
+
+
+    var data= [];
+    function onSubmit() {
+        data = document.getElementById('array_of_people').value;
+
+        initSniped();
+        if ('indexedDB' in window) {
+            initDatabase(data);
+
+            console.log('This browser support IndexedDB');
+        }
+        else {
+            console.log('This browser doesn\'t support IndexedDB');
+        }
+    }
+
+
+
+    // var dbPromise = idb.open
+
+};
+
 function initSniped() {
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('./service-worker.js')
-            .then(function() {
+            .then(function () {
                 console.log('Service Worker Registered');
             })
-            .catch(function(error) {
-                console.log('Service Worker NOT Registered '+ error.message);
+            .catch(function (error) {
+                console.log('Service Worker NOT Registered ' + error.message);
             });
     }
 
-    //check for indexeddb support
-    if ('indexedDB' in window) {
-        initDatabase();
-        console.log('This browser support IndexedDB');
-    }
-    else {
-        console.log('This browser doesn\'t support IndexedDB');
-    }
-
-    // var dbPromise = idb.open
-}
+};
 
 /**
  * When the client gets off-line, it shows an off line warning to the user
@@ -61,88 +76,47 @@ const SNIPED_STORE_USERS = 'users';
 const SNIPED_STORE_PHOTOS = 'uploads';
 
 
-var imagesInital = [
-    {"imagePath":"public/uploads/photo-1558014347932.jpg","profileID":"109272662587431712473","event":"Glasto","comment":"This is glasto","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014355718.jpg","profileID":"109272662587431712473","event":"Cohella","comment":"This is Cohella","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014361498.jpg","profileID":"109272662587431712473","event":"Leeds fest","comment":"This is Leeds fest","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014368686.jpg","profileID":"109272662587431712473","event":"Wireless","comment":"This is Wireless","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014377366.jpg","profileID":"109272662587431712473","event":"Woo Hah0","comment":"This is Woo Hah0","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014383941.jpg","profileID":"109272662587431712473","event":"Reading","comment":"This is Reading","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014390268.jpg","profileID":"109272662587431712473","event":"Londom fest","comment":"This is Londom","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014400975.jpg","profileID":"109272662587431712473","event":"Tramlines","comment":"This is Tramlines","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014407541.jpg","profileID":"109272662587431712473","event":"Didsbury fest","comment":"This is Didsbury","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014414354.jpg","profileID":"109272662587431712473","event":"Junaids world","comment":"This is Junaids","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014420321.jpg","profileID":"109272662587431712473","event":"Outlook","comment":"This is glasto","location":"Outlook","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014427095.jpg","profileID":"109272662587431712473","event":"Social club","comment":"This is Social","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014434752.jpg","profileID":"109272662587431712473","event":"Manchesr academy","comment":"This is Manchesr","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014441701.jpg","profileID":"109272662587431712473","event":"Birmingham disco","comment":"This is disco","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014449385.jpg","profileID":"109272662587431712473","event":"New york party","comment":"This is york","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014459099.jpg","profileID":"109272662587431712473","event":"Japan party","comment":"This is Japan","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014466729.jpg","profileID":"109272662587431712473","event":"Glasto v2","comment":"This is v2","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014480929.jpg","profileID":"109272662587431712473","event":"Austrila partyier","comment":"This is partyier","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014487491.jpg","profileID":"109272662587431712473","event":"India festival","comment":"This is India","location":"","dateTime":"06/12/9412:15"},
-    {"imagePath":"public/uploads/photo-1558014496801.jpg","profileID":"109272662587431712473","event":"Last festival location","comment":"This is Last","location":"","dateTime":"06/12/9412:15"}
-]
 
 
+function initDatabase(data) {
+    var db;
 
-function initDatabase() {
-    console.log('checkcheckchekccc')
-    dbPromise = idb.openDb(SNIPED_DB_NAME, 1, function (upgradeDb) {
+    var openRequest = indexedDB.open(SNIPED_DB_NAME, 1);
 
-        // if (!upgradeDb.objectStoreNames.contains(SNIPED_STORE_USERS)) {
-        //     var userOS = upgradeDb.createObjectStore(SNIPED_STORE_USERS, {keyPath: 'profileId'});
-        //     userOS.createIndex('displayName', 'displayName', {unique: false, multiEntry: true});
-        // }
-        if (!upgradeDb.objectStoreNames.contains(SNIPED_STORE_PHOTOS)) {
-            var uploadOS = upgradeDb.createObjectStore(SNIPED_STORE_PHOTOS, {autoIncrement: true});
-            uploadOS.createIndex('imagePath', 'imagePath', {unique: false, multiEntry: true});
-            uploadOS.createIndex('profileId', 'profileId', {unique: false, multiEntry: true});
-            uploadOS.createIndex('event', 'event', {unique: false, multiEntry: true});
-            uploadOS.createIndex('comment', 'comment', {unique: false, multiEntry: true});
-            uploadOS.createIndex('location', 'location', {unique: false, multiEntry: true});
-            uploadOS.createIndex('dateTime', 'dateTime', {unique: false, multiEntry: true});
+    openRequest.onupgradeneeded = function (e) {
+        var db = e.target.result;
+        console.log('running onupgradeneeded');
+        if (!db.objectStoreNames.contains(SNIPED_STORE_PHOTOS)) {
+
+            var uploadOS = db.createObjectStore(SNIPED_STORE_PHOTOS, {autoIncrement: true})
+
         }
-
+    };
+    openRequest.onsuccess = function (e) {
+        console.log('running onsuccess');
+        db = e.target.result;
         addItem();
-
-        // if (dbPromise) {
-        //     dbPromise.then(async db => {
-        //         var tx = db.transaction(uploadOS, 'readwrite');
-        //         var store = tx.objectStore(uploadOS);
-        //         await store.put(imagesInital);
-        //         return tx.complete;
-        //     }).then(function() {
-        //         console.log('added item to the store! '+ JSON.stringify(data));
-        //     });
-        // }
-
-    });
-
-    db = dbPromise.result;
+    };
+    openRequest.onerror = function (e) {
+        console.log('onerror!');
+        console.dir(e);
+    };
 
     function addItem() {
         var transaction = db.transaction([SNIPED_STORE_PHOTOS], 'readwrite');
         var store = transaction.objectStore(SNIPED_STORE_PHOTOS);
-        var item = {
-            imagePath: "public/uploads/photo-1558014347932.jpg",
-            profileId: "109272662587431712473",
-            event: "Glasto",
-            comment: "This is glasto",
-            location: "",
-            dateTime: "06/12/9412:15"
-        };
 
-        var request = store.add(item)
 
-        request.onsuccess = function(e) {
-            console.log('Woot! Did it');
-        };
+        JSON.parse(data).forEach(function (obj) {
 
+            var request = store.add(obj)
+
+            request.onsuccess = function (e) {
+                console.log('Woot! Did it');
+
+            };
+
+
+        });
     }
-
-
-
 }
-
-
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var profileID ="";
 const multer = require('multer');
 
 
+
 /* Adds mongodb package */
 const MongoClient = require('mongodb').MongoClient;
 // Connection URL
@@ -38,7 +39,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sniped!' });
 });
 
-function searchMongoToIndex(eventSearch) {
+function searchMongoToIndex(eventSearch, req, res) {
 
 
     /* Adds mongodb package */
@@ -66,7 +67,34 @@ function searchMongoToIndex(eventSearch) {
 
             collection.find({'event':new RegExp(eventName)}).toArray(function(err, result) {
                 if (err){ throw err}else{
-                    console.log(result);
+
+                    //get indeximages from indexed db
+                    test = [{"imagePath":"public/uploads/photo-1558014347932.jpg","profileID":"109272662587431712473","event":"glasto","comment":"This is glasto","location":"","dateTime":"06/12/9412:15"}]
+                    //update images
+                    allPhotos = "";
+                    // for each photo in array append it to html
+
+                    result.forEach(function(obj) {console.log(typeof    obj.imagePath.slice(7).slice(0, -4));
+                        console.log(obj.imagePath.slice(7).slice(0, -4));
+                        var photos = "      <div class=\"thumb_images col-md-4\">\n" +
+                            "\n" +
+                            "                    <a href=\"#image1\" data-toggle=\"modal\" data-target=\"#image1\">\n" +
+                            "                    <img class=\"card-img-top\"\n" +
+                            "                         data-src=\"holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail\"\n" +
+                            "                         alt=\"Thumbnail [100%x225]\" style=\"height: 225px; width: 100%; display: block;\"\n" +
+                            "                         src=" +
+                            "\"" +
+                            obj.imagePath.slice(7).slice(0, -4) +
+                            "\"\n" +
+                            "                         data-holder-rendered=\"true\"></a>\n" +
+                            "\n" +
+                            "                </div>\n";
+                        allPhotos+= photos;
+                    });
+
+
+                    res.render('profile', {user: req.user, title: 'Welcome back!',DisplayName:profileID.displayName,image :profileID.Image, searchPhotos: allPhotos, profile:"", results:""});
+
 
                     // Result is the ouput of the search term for events
                 };
@@ -81,7 +109,7 @@ function searchMongoToIndex(eventSearch) {
 };
 
 
-function userProfileMongoToIndex(ProfileID) {
+function userProfileMongoToIndex(ProfileIDNum, req, res, profileID) {
 
 
     /* Adds mongodb package */
@@ -105,11 +133,13 @@ function userProfileMongoToIndex(ProfileID) {
         const db = client.db(dbName);
         const collection = db.collection('upload');
         // existingUser = collection.find({profileID:ProfileID})
-        var userName = ProfileID;
+            //var userName = ProfileID;
 
-        collection.find({'profileID':userName}).toArray(function(err, result) {
+        collection.find({'profileID':ProfileIDNum}).toArray(function(err, result) {
             if (err){ throw err}else{
-                //console.log(result);
+
+                res.render('profile', { user: req.user, title: 'Welcome back!',DisplayName:profileID.displayName,image : profileID.Image, searchPhotos:"", results:JSON.stringify(result)});
+
 
                 // Result is an array of json. add it to indexdb.
 
@@ -131,67 +161,47 @@ router.post('/logout', (req, res) => {
 });
 
 router.get('/profile', function(req, res){
-    userProfileMongoToIndex("109272662587431712473");
-    if(req.session.user == null){res.redirect("/");};
-    console.log(req.session.profileID);
     profileID = req.session.user;
-    console.log(profileID.Image);
 
-    res.render('profile', { user: req.user, title: 'Welcome back!',DisplayName:profileID.displayName,image : profileID.Image, searchPhotos:""});
+    userProfileMongoToIndex("109272662587431712473", req, res, profileID);
+
+    if(req.session.user == null){res.redirect("/");};
+
+
 
 });
 
 router.post('/profile', function(req, res) {
 
-    searchMongoToIndex(req.body.searchInput.toLowerCase());
-    //get indeximages from indexed db
-    test = [{"imagePath":"public/uploads/photo-1558014347932.jpg","profileID":"109272662587431712473","event":"glasto","comment":"This is glasto","location":"","dateTime":"06/12/9412:15"}]
-    //update images
-    allPhotos = "";
-    // for each photo in array append it to html
-
-    test.forEach(function(obj) {console.log(obj.imagePath.slice(7).slice(0, -4));
-        var photos = "      <div class=\"thumb_images col-md-4\">\n" +
-            "\n" +
-            "                    <a href=\"#image1\" data-toggle=\"modal\" data-target=\"#image1\">\n" +
-            "                    <img class=\"card-img-top\"\n" +
-            "                         data-src=\"holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail\"\n" +
-            "                         alt=\"Thumbnail [100%x225]\" style=\"height: 225px; width: 100%; display: block;\"\n" +
-            "                         src=" +
-            "\"" +
-            "uploads/photo-1558014347932" +
-            "\"\n" +
-            "                         data-holder-rendered=\"true\"></a>\n" +
-            "\n" +
-            "                </div>\n";
-        allPhotos+= photos;
-    });
-
-
-    res.render('profile', {user: req.user, title: 'Welcome back!',DisplayName:profileID.displayName,image :profileID.Image, searchPhotos: allPhotos});
-
+    searchMongoToIndex(req.body.searchInput.toLowerCase(), req, res);
 
 
 });
+
 /* POST event image. */
 router.post('/upload', upload.single('photo'), (req, res, next) => {
+
+
+    console.log(req.body.eventName);
+
     client.connect(function(err) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
         const db = client.db(dbName);
         insertDocuments(db, ('public/uploads/' + req.file.filename +'.jpg'), () => {
             res.json({'message': 'File uploaded successfully'});
-        });
+        }, req, res);
     });
+
 });
 module.exports = router;
 
 /* Function to inset one file to upload collection */
-const insertDocuments = function(db,filePath, callback) {
+const insertDocuments = function(db,filePath, callback, req, res) {
     // Get the documents collection
     const collection = db.collection('upload');
     // Insert some documents
-    collection.insertOne({'imagePath':filePath, 'profileID': profileID.profileID}, function(err, result) {
+    collection.insertOne({'imagePath':filePath, 'profileID': profileID.profileID, 'event': req.body.eventName, location:req.body.location, comment:req.body.comment, results:""}, function(err, result) {
         assert.equal(err, null);
         callback(result);
     });
